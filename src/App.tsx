3e5f0502a0fa816e08075916ef21b64ba32b97ac
김민지 <alswlk0306@gmail.com>
2025-02-11 13:49:41 +0900

    Design: 메인(구매하기) 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
