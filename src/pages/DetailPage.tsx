7b26d894e4d632995d4c0d8b71253edf43782912
김민지 <alswlk0306@gmail.com>
2025-02-19 23:19:10 +0900

    Design: 여백 값 수정

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -12,7 +12,7 @@ function DetailPage() {
   }
 
   return (
-    <div className="flex flex-col justify-between min-h-screen max-w-md mx-auto px-4 space-y-4 bg-white pt-28 pb-8">
+    <div className="flex flex-col justify-between min-h-screen max-w-md mx-auto px-4 space-y-4 bg-white pt-24 pb-8">
       <div>
         <div className="aspect-square bg-blue-100 p-6 rounded-lg text-center">
           <img src={`${book.imageUrls}`} />
