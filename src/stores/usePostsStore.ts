4ad7214c97f5e91b0968bb3c83a25f8bd7832925
김민지 <alswlk0306@gmail.com>
2025-03-03 14:00:45 +0900

    Fix: 서적 전체 조회 헤더 제거

--- src/stores/usePostsStore.ts
+++ src/stores/usePostsStore.ts
@@ -13,19 +13,12 @@ const usePostsStore = create<PostsState>((set, get) => ({
     set({ loading: true, error: null });
   
     try {
-      // 로컬 스토리지에서 JWT 토큰 가져오기
-      const token = localStorage.getItem("accessToken");
-  
-      // 요청 헤더에 Authorization 추가
-      const response = await api.get("books/", {
-        headers: {
-          Authorization: `Bearer ${token}`, // JWT 토큰 추가
-        },
-      });
+      const response = await api.get("books/");
   
       const apiBooks = response.data.map((book: any) => ({
         book_id: book.id,
         title: book.title,
+        chatLink: book.chatLink,
         price: book.price,
         description: book.description,
         saleStatus: book.status === "FOR_SALE",
