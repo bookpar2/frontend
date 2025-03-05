acd764493b880ec9028caafaf09899a84886cfa0
김민지 <alswlk0306@gmail.com>
2025-03-05 11:19:14 +0900

    Fix: HashRouter로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import Navbar from './components/Navbar';
