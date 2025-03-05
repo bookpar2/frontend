b400d185792d33d2a260384ff5b3a2f3f55b3b2c
김민지 <alswlk0306@gmail.com>
2025-03-05 11:43:15 +0900

    Feat: 세션 자동 갱신

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -10,37 +10,16 @@ interface UserState {
   access: string | null;
   isLoggedIn: boolean;
   setUser: (userData: Partial<UserState>) => void;
+  refreshAccessToken: () => Promise<void>;
+  autoLogin: () => void;
   logout: () => void;
 }
 
+// sessionStorage에서 초기 값 가져오기
 const storedUser = sessionStorage.getItem("user");
-const initialUser = storedUser ? JSON.parse(storedUser) : {
-  id: null,
-  name: null,
-  school_email: null,
-  major: null,
-  student_id: null,
-  refresh: null,
-  access: null,
-  isLoggedIn: false,
-};
-
-const useUserStore = create<UserState>((set) => ({
-  ...initialUser, 
-
-  setUser: (userData) => {
-    const newUserState = {
-      ...userData,
-      isLoggedIn: !!userData.refresh,
-    };
-
-    sessionStorage.setItem("user", JSON.stringify(newUserState)); // sessionStorage에 저장
-    set(newUserState);
-  },
-
-  logout: () => {
-    sessionStorage.removeItem("user");
-    set({
+const initialUser: UserState = storedUser
+  ? JSON.parse(storedUser)
+  : {
       id: null,
       name: null,
       school_email: null,
@@ -49,8 +28,37 @@ const useUserStore = create<UserState>((set) => ({
       refresh: null,
       access: null,
       isLoggedIn: false,
-    });
-  },
-}));
+    };
+
+    const useUserStore = create<UserState>((set) => ({
+      ...initialUser,
+    
+      setUser: (userData) => {
+        const newUserState = {
+          ...useUserStore.getState(), // 기존 상태 유지
+          ...userData,
+          isLoggedIn: !!userData.refresh,
+        };
+    
+        sessionStorage.setItem("user", JSON.stringify(newUserState)); // sessionStorage에 저장
+        set(newUserState);
+      },
+    
+      logout: () => {
+        sessionStorage.removeItem("user");
+        set({
+          id: null,
+          name: null,
+          school_email: null,
+          major: null,
+          student_id: null,
+          refresh: null,
+          access: null,
+          isLoggedIn: false,
+        });
+      },
+    }
+  )
+);    
 
 export default useUserStore;
