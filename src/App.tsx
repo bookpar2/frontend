1e04b3d488bc6cda40be5b0b317c2484ec5c7fe8
김민지 <alswlk0306@gmail.com>
2026-02-26 00:10:49 +0900

    refactor: 레이아웃 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,73 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import MainPage from './pages/MainPage';
-import LoginPage from './pages/LoginPage';
-import Navbar from './components/Navbar';
-import RegisterPage from './pages/RegisterPage';
-import SellPage from './pages/SellPage';
-import DetailPage from './pages/DetailPage';
-import MyPage from './pages/MyPage';
-import useUserStore from './stores/useUserStore';
-import { ReactNode } from 'react';
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { ReactNode } from "react";
+
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import SellPage from "./pages/SellPage";
+import DetailPage from "./pages/DetailPage";
+import MyPage from "./pages/MyPage";
+
+import useUserStore from "./stores/useUserStore";
+import DefaultLayout from "./layouts/DefaultLayout";
+
+import "./App.css";
 
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 
-// 로그인 상태를 체크하는 ProtectedRoute 컴포넌트
-function ProtectedRoute({ children }: ProtectedRouteProps) {
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const { isLoggedIn } = useUserStore();
-
-  if (!isLoggedIn) {
-    // 로그인하지 않은 경우, 로그인 페이지로 리디렉션
-    return <Navigate to="/login" replace />;
-  }
+  if (!isLoggedIn) return <Navigate to="/login" replace />;
   return children;
 }
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="w-full min-w-[375px] min-h-screen overflow-x-hidden">
-        <Navbar />
+      <div className="w-full min-w-[375px] h-dvh overflow-x-hidden">
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<MainPage />} />
 
-          {/* 로그인한 사용자만 접근 가능 */}
-          <Route
-            path="/sell"
-            element={
-              <ProtectedRoute>
-                <SellPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/edit/:book_id"
-            element={
-              <ProtectedRoute>
-                <SellPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/detail/:book_id"
-            element={
-              <ProtectedRoute>
-                <DetailPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/mypage"
-            element={
-              <ProtectedRoute>
-                <MyPage />
-              </ProtectedRoute>
-            }
-          />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+
+            {/* 로그인한 사용자만 */}
+            <Route
+              path="/sell"
+              element={
+                <ProtectedRoute>
+                  <SellPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/edit/:book_id"
+              element={
+                <ProtectedRoute>
+                  <SellPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/detail/:book_id"
+              element={
+                <ProtectedRoute>
+                  <DetailPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/mypage"
+              element={
+                <ProtectedRoute>
+                  <MyPage />
+                </ProtectedRoute>
+              }
+            />
+          </Route>
         </Routes>
       </div>
     </Router>
